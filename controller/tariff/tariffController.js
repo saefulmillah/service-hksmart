af8949f9b00484705a82d7f9956a93e2b5738d9f
saeful.millah0607 <saeful.millah0607@gmail.com>
2019-09-06 10:39:13 +0700

    cek console parsing req body

--- controller/tariff/tariffController.js
+++ controller/tariff/tariffController.js
@@ -4,15 +4,11 @@ var Tariff = require('../../model/tariffModel.js');
 
 exports.read_tariff_by_filter = function(req, res) {
   var a = req.body
-  var parsing = a.jsonReqTariff
-  console.log(parsing)
-  return
-  // console.log(get_tariff.GATE_ORIGIN_ID);
-  // return
-
-  Tariff.getTariffByFilter(get_tariff, function (err, result) {
+  var parseReq = a.jsonReqTariff
+  // console.log(parseReq)
+  Tariff.getTariffByFilter(parseReq, function (err, result) {
   	if (err) {
-  		res.send(err);
+  		res.send(err)
   	} else {
   		res.status(200).json({
   			status : "success",
--- model/tariffModel.js
+++ model/tariffModel.js
@@ -12,21 +12,16 @@ var Tariff = function(tariff){
 
 
 Tariff.getTariffByFilter = function getTariffByFilter(query, result) {
-    // var obj = JSON.parse(query)
-    console.log(query)
-    console.log(query['GOL_ID'])
-    
-    return
-    // console.log("SELECT * FROM map_tariff WHERE GATE_ORIGIN_ID ="+ query.GATE_ORIGIN_ID+"  AND GATE_DESTINATION_ID = "+query.GATE_DESTINATION_ID+" AND GOL_ID = "+query.GOL_ID )
-        // sql.query("SELECT * FROM map_tariff WHERE GATE_ORIGIN_ID ="+ query.GATE_ORIGIN_ID+"  AND GATE_DESTINATION_ID = "+query.GATE_DESTINATION_ID+" AND GOL_ID = "+query.GOL_ID , function (err, res) {             
-        //     if(err) {
-        //         console.log("error: ", err);
-        //         result(err, null);
-        //     }
-        //     else{
-        //         result(null, res);
-        //     }
-        // });  
+        // console.log("SELECT * FROM map_tariff WHERE GATE_ORIGIN_ID ="+ query.GATE_ORIGIN_ID+"  AND GATE_DESTINATION_ID = "+query.GATE_DESTINATION_ID+" AND GOL_ID = "+query.GOL_ID )
+    sql.query("SELECT * FROM map_tariff WHERE GATE_ORIGIN_ID ="+ query.GATE_ORIGIN_ID+"  AND GATE_DESTINATION_ID = "+query.GATE_DESTINATION_ID+" AND GOL_ID = "+query.GOL_ID , function (err, res) {             
+        if(err) {
+            console.log("error: ", err);
+            result(err, null);
+        }
+        else{
+            result(null, res);
+        }
+    });  
 }
 
 module.exports= Tariff;
