37790a6c6a318dc854df3ecec139063e473905a8
saeful.millah0607 <saeful.millah0607@gmail.com>
2019-09-06 10:06:39 +0700

    cek console parsing req body

--- model/tariffModel.js
+++ model/tariffModel.js
@@ -13,8 +13,9 @@ var Tariff = function(tariff){
 
 Tariff.getTariffByFilter = function getTariffByFilter(query, result) {
     // var obj = JSON.parse(query)
-    console.log(query[GOL_ID])
-    console.log(obj)
+    console.log(query)
+    console.log(query['GOL_ID'])
+    
     return
     // console.log("SELECT * FROM map_tariff WHERE GATE_ORIGIN_ID ="+ query.GATE_ORIGIN_ID+"  AND GATE_DESTINATION_ID = "+query.GATE_DESTINATION_ID+" AND GOL_ID = "+query.GOL_ID )
         // sql.query("SELECT * FROM map_tariff WHERE GATE_ORIGIN_ID ="+ query.GATE_ORIGIN_ID+"  AND GATE_DESTINATION_ID = "+query.GATE_DESTINATION_ID+" AND GOL_ID = "+query.GOL_ID , function (err, res) {             
