59e785ea9cae0aa26bf77ac04479eab462613224
saeful.millah0607 <saeful.millah0607@gmail.com>
2019-09-06 10:36:40 +0700

    cek console parsing req body

--- controller/tariff/tariffController.js
+++ controller/tariff/tariffController.js
@@ -3,10 +3,9 @@
 var Tariff = require('../../model/tariffModel.js');
 
 exports.read_tariff_by_filter = function(req, res) {
-  var get_tariff = req.body
-  // var reqParse = JSON.parse(get_tariff);
-  // console.log(reqParse)
-  console.log(get_tariff)
+  var a = req.body
+  var parsing = a.jsonReqTariff
+  console.log(parsing)
   return
   // console.log(get_tariff.GATE_ORIGIN_ID);
   // return
