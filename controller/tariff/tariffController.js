66772781df066475807848727e11c7cfc06fd4bc
saeful.millah0607 <saeful.millah0607@gmail.com>
2019-09-06 15:34:58 +0700

    cek console parsing req body

--- controller/tariff/tariffController.js
+++ controller/tariff/tariffController.js
@@ -4,9 +4,10 @@ var Tariff = require('../../model/tariffModel.js');
 
 exports.read_tariff_by_filter = function(req, res) {
   var a = req.body
-  var parseReq = a.jsonReqTariff
-  // console.log(parseReq)
-  Tariff.getTariffByFilter(parseReq, function (err, result) {
+  var b = a.jsonReqTariff
+  var cleanb = b.replace(/"/g, "");
+  // console.log(b)
+  Tariff.getTariffByFilter(b, function (err, result) {
   	if (err) {
   		res.send(err)
   	} else {
