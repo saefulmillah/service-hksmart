a5dcdb46826326e746729b7357eb1e49b169e3fc
saeful.millah0607 <saeful.millah0607@gmail.com>
2019-09-09 09:48:39 +0700

    add new file tariffController.js

---
+++ controller/tariff/tariffController.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var Tariff = require('../../model/tariffModel.js');
+
+exports.read_tariff_by_filter = function(req, res) {
+  var a = req.body
+  var b = a.jsonReqTariff
+  // var cleanb = b.replace(/['"]+/g, '');
+  // console.log(req.body)
+  // console.log(a)
+  console.log(b)
+  // return
+  Tariff.getTariffByFilter(b, function (err, result) {
+  	if (err) {
+  		res.send(err)
+  	} else {
+		console.log(result)
+  		res.status(200).json({
+  			status : "success",
+  			data : result 
+  		});
+  	}
+  });
+};
