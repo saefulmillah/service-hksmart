ae233b1f29a5113dcddb2d1e1027a99c70cf8a5a
saeful.millah0607 <saeful.millah0607@gmail.com>
2019-09-16 13:21:19 +0700

    service get all restarea

--- controller/restarea/restareaController.js
+++ controller/restarea/restareaController.js
@@ -4,11 +4,8 @@ var RestArea = require('../../model/restareaModel.js');
 
 exports.list_all_restarea = function(req, res) {
   RestArea.getAllRestArea(function(err, restarea) {
-
-    console.log('controller')
     if (err)
       res.send(err);
-      console.log('res', restarea);
       res.send({ restarea });
   });
 };
