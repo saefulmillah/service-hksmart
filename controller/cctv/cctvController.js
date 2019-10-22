2829710aac13d0bb3bab7c360a60e0b96f9e6bc3
saeful.millah0607 <saeful.millah0607@gmail.com>
2019-10-22 10:31:38 +0700

    add module cctv

--- controller/cctv/cctvController.js
+++ controller/cctv/cctvController.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var Tariff = require('../../model/cctvModel.js');
+var Cctv = require('../../model/cctvModel.js');
 
 exports.read_cctv_byBranch = function(req, res) {
-  Tariff.getCctvByBranch(req.params.branchID, function (err, cctv) {
+  Cctv.getCctvByBranch(req.params.branchID, function (err, cctv) {
   	if (err)
     res.send(err);
     res.json({cctv});
