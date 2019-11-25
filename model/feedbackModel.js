04c670adc68fd8db2c356a52a0fa401d1b7ccaf8
saeful.millah0607 <saeful.millah0607@gmail.com>
2019-11-25 16:56:47 +0700

    insert feedback

--- model/feedbackModel.js
+++ model/feedbackModel.js
@@ -24,6 +24,7 @@ Feedback.insertFeedback = function (query, result) {
 		id_ruas : a.id_ruas,
 		answer : a.answer
 	}
+	console.log("request > b", a)
 	console.log("request > b", b)
 	sql.query(q, b, function (err, res) {
 		if (err) {
